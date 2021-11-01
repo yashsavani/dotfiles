7207bec493cfa8864466b0d3d05a5e1b5055ad31
Yash Savani <yash.savani@gmail.com>
2021-11-01 12:39:14 -0400

    new cmp autopairs config

--- nvim/lua/plugins/cmp_config.lua
+++ nvim/lua/plugins/cmp_config.lua
@@ -1,4 +1,5 @@
 local cmp = require'cmp'
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -60,13 +61,4 @@ cmp.setup({
     },
 })
 
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  }
-})
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
